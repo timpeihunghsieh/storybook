4872e06651ece5b8ff86f6feb9a0bd7b675931dc
Tim Hsieh <timpeihunghsieh@gmail.com>
2021-07-11 15:42:45 -0400

    Move navbar component under components/ dir

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
