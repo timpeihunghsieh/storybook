915db21835f6b689e3b89c7e94b9dc2d4b16b9f2
Tim Hsieh <timpeihunghsieh@gmail.com>
2021-07-14 19:04:20 -0400

    Remove various console log

--- backend/routes/stories.js
+++ backend/routes/stories.js
@@ -7,15 +7,11 @@ const Story = require('../models/Story');
 // @desc  Process add form
 // @route POST /stories
 router.post('/', ensureAuth, async (req, res) => {
-  console.log("Processing add story");
   try {
-    console.log(req.body);
     req.body.user = req.user.id;
     const story = await Story.create(req.body);
     res.json({success: true, msg: 'Story added'});
   } catch (err) {
-    console.log("Cannot add story");
-    console.error(err);
     res.json({success: false, msg: 'Cannot add story'});
   }
 });
@@ -23,7 +19,6 @@ router.post('/', ensureAuth, async (req, res) => {
 // @desc  Show all stories
 // @route GET /stories
 router.get('/', ensureAuth, async (req, res) => {
-  console.log("Processing show all stories");
   try {
     const stories = await Story.find({status: 'public'})
         .populate('user')
@@ -32,7 +27,6 @@ router.get('/', ensureAuth, async (req, res) => {
     res.json(stories);
   } catch (err) {
     // TODO(timhsieh): Figure out what to do when error.
-    console.log("Get all stories error");
     console.log(err);
   }
 });
@@ -46,13 +40,11 @@ router.get('/one-story/:id', ensureAuth, async (req, res) => {
       .lean();
 
     if (!story) {
-      console.log("Get a single story error");
       res.json({});
     }
     res.json(story);
   } catch (err) {
     // TODO(timhsieh): Figure out what to do when error.
-    console.log("Get a single story error");
     console.error(err);
   }
 });
@@ -64,7 +56,6 @@ router.get('/my-stories', ensureAuth, async (req, res) => {
     const stories = await Story.find({ user: req.user.id }).lean();
 
     if (!stories) {
-      console.log("Get my stories error");
       res.json({});
     }
     res.json(stories);
@@ -79,15 +70,12 @@ router.get('/my-stories', ensureAuth, async (req, res) => {
 // @route PUT /stories/:id
 router.put('/:id', ensureAuth, async (req, res) => {
   try {
-    console.log("Attempt update story");
     let story = await Story.findById(req.params.id).lean();
     if (!story) {
-      console.log("Update story error");
       res.json({});
     }
 
     if (story.user != req.user.id) {
-      console.log("Update story error");
       res.json({});
     } else {
       story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
@@ -95,7 +83,6 @@ router.put('/:id', ensureAuth, async (req, res) => {
         runValidators: true
       });
 
-      console.log("Update story success");
       res.json({success: true});
     }
   } catch (error) {
@@ -111,12 +98,10 @@ router.delete('/:id', ensureAuth, async (req, res) => {
     let storyId = req.params.id
     let story = await Story.findById(storyId).lean();
     if (!story) {
-      console.log("Delete story error");
       return;
     }
 
     if (story.user != req.user.id) {
-      console.log("Delete story error");
       return;
     } else {
       await Story.remove({_id: storyId});
@@ -125,7 +110,6 @@ router.delete('/:id', ensureAuth, async (req, res) => {
       res.status(200);
     }
   } catch (err) {
-    console.log("Delete story error");
     console.error(err);
   }
 });
@@ -134,7 +118,6 @@ router.delete('/:id', ensureAuth, async (req, res) => {
 // @desc  GET this user's uid
 // @route GET /stories/myuid
 router.get('/myuid', ensureAuth, async (req, res) => {
-  console.log("Processing show myuid");
   try {
     res.json(req.user.id);
   } catch (err) {
